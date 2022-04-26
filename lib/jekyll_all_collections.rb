97f46f2e0b2b6412b628c2af28d7023ebfb68842
Mike Slinn <mslinn@micronauticsresearch.com>
2022-04-26 08:50:56 -0400

    Works but ...

--- lib/jekyll_all_collections.rb
+++ lib/jekyll_all_collections.rb
@@ -8,14 +8,20 @@ require_relative "jekyll_all_collections/version"
 module JekyllAllCollections
   # @param payload [Hash] contains final values of variables after rendering the entire site (useful for sitemaps, feeds, etc).
   Jekyll::Hooks.register(:site, :post_render, priority: :high) do |site, _payload|
-    @log_site.info { "Jekyll::Hooks.register(:site, :post_render) invoked." }
+    @logger = PluginMetaLogger.instance.new_logger(self, PluginMetaLogger.instance.config)
+    @logger.info { "JekyllAllCollections invoked Jekyll::Hooks.register(:site, :post_render, priority: :high)." }
 
-    site.class.module_eval { attr_accessor :all_collections}
+    site.class.module_eval { attr_accessor :all_collections }
     site.all_collections =
       site.collections
           .values
           .map { |x| x.class.method_defined?(:docs) ? x.docs : x }
           .flatten
+  end
+
+  Jekyll::Hooks.register(:site, :post_write) do |site|
+    _x = site.all_collections
+    puts "site.all_collections exists".red
   end
 
   PluginMetaLogger.instance.logger.info { "Loaded JekyllAllCollections v#{JekyllAllCollectionsVersion::VERSION} plugin." }
