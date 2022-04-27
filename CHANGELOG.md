189970d9852dc6c2f847ff0944a9526006f3da3b
Mike Slinn <mslinn@micronauticsresearch.com>
2022-04-27 11:22:45 -0400

    Changed invocation from a :site hook to an idempotent method invocation

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,2 +1,5 @@
+## 0.1.1 / 2022-04-27
+  * Changed invocation from a `:site` hook to an idempotent method invocation.
+
 ## 0.1.0 / 2022-04-26
   * Initial version published
--- lib/jekyll_all_collections.rb
+++ lib/jekyll_all_collections.rb
@@ -4,12 +4,15 @@ require "jekyll"
 require "jekyll_plugin_logger"
 require_relative "jekyll_all_collections/version"
 
-# Creates a property called site.all_collections
+# Creates a property called site.all_collections if it does not already exist
 module JekyllAllCollections
-  # @param payload [Hash] contains final values of variables after rendering the entire site (useful for sitemaps, feeds, etc).
-  Jekyll::Hooks.register(:site, :post_render, priority: :high) do |site, _payload|
-    @logger = PluginMetaLogger.instance.new_logger(self, PluginMetaLogger.instance.config)
-    @logger.info { "JekyllAllCollections invoked Jekyll::Hooks.register(:site, :pre_render, priority: :high)." }
+  @logger = PluginMetaLogger.instance.new_logger(self, PluginMetaLogger.instance.config)
+
+  def maybe_compute_all_collections(site)
+    @logger.debug { "JekyllAllCollections.maybe_compute_all_collections invoked" }
+    return if site.class.method_defined? :all_collections
+
+    @logger.debug { "JekyllAllCollections.maybe_compute_all_collections creating site.all_collections" }
 
     site.class.module_eval { attr_accessor :all_collections }
     site.all_collections =
@@ -19,10 +22,7 @@ module JekyllAllCollections
           .flatten
   end
 
-  Jekyll::Hooks.register(:site, :post_write) do |site|
-    _x = site.all_collections
-    puts "site.all_collections exists".red
-  end
+  module_function :maybe_compute_all_collections
 
   PluginMetaLogger.instance.logger.info { "Loaded JekyllAllCollections v#{JekyllAllCollectionsVersion::VERSION} plugin." }
 end
