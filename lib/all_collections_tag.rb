104097803c943eb826609f013555a452d7165a63
Mike Slinn <mslinn@mslinn.com>
2023-02-07 13:49:16 -0500

    -

--- lib/all_collections_tag.rb
+++ lib/all_collections_tag.rb
@@ -6,20 +6,32 @@ require 'jekyll_plugin_support'
 # @license SPDX-License-Identifier: Apache-2.0
 module AllCollectionsTag
   PLUGIN_NAME = 'all_collections'.freeze
+  CRITERIA = %w[date destination draft label last_modified path relative_path title type url].freeze
 
   class AllCollectionsTag < JekyllSupport::JekyllTag
     # Method prescribed by JekyllTag.
     # @return [String]
     def render_impl
       AllCollectionsHooks.compute(@site) unless @site.class.method_defined? :all_collections
-      sort_by_age
+
+      sort_by = @helper.parameter_specified?('sort_by') || 'date:asc'
+      sort_lambda = create_lambda(verify_sort_by_type(sort_by))
+      generate_output(sort_lambda)
     end
 
-    def sort_by_age
+    private
+
+    def create_lambda(criteria)
+      criteria.each do |c|
+        lambda(&:date)
+      end
+    end
+
+    def generate_output(sort_lambda)
       <<~END_TEXT
         <h2 id="posts">Posts Sorted By Age</h2>
         <div class="posts">
-        #{(@site.all_collections.sort_by(&:date).map do |post|
+        #{(@site.all_collections.sort(sort_lambda).map do |post|
              draft = Jekyll::Draft.draft_html post
              date = post.data['date'].strftime('%Y-%m-%d')
              href = "<a href='#{post.url}'>#{post.title}</a>"
@@ -27,6 +39,18 @@ module AllCollectionsTag
            end).join("\n")}
         </div>
       END_TEXT
+    end
+
+    def verify_sort_by_type(sort_by)
+      if @sort_by.is_a? Array
+        sort_by
+      elsif sort_by.is_a? Enumerable
+        sort_by.to_a
+      elsif sort_by.is_a? String
+        [sort_by]
+      else
+        abort "Error: sort_by was specified as '#{sort_by}'; it must either be a string or an array of strings"
+      end
     end
   end
 end
--- spec/jekyll_all_collections_spec.rb
+++ spec/jekyll_all_collections_spec.rb
@@ -1,5 +1,5 @@
-require "jekyll"
-require_relative "../lib/jekyll_all_collections"
+# require "jekyll"
+# require_relative "../lib/jekyll_all_collections"
 
 class Obj
   attr_reader :v1, :v2, :v3
@@ -11,26 +11,45 @@ class Obj
   end
 end
 
-# See https://stackoverflow.com/a/16628808/553865
-RSpec.describe(JekyllAllCollections) do
+RSpec.describe(Array) do
   let(:o1) { Obj.new(1, 1, 1) }
   let(:o2) { Obj.new(2, 1, 1) }
   let(:o3) { Obj.new(2, 2, 1) }
   let(:o4) { Obj.new(3, 2, 1) }
   let(:objs) { [o1, o2, o3, o4] }
 
+  # See https://ruby-doc.org/3.2.0/Comparable.html
+  it "uses comparators properly" do
+    expect([o1.v1] <=> [o2.v1]).to eq(-1)
+    expect([o1.v1, o1.v2] <=> [o2.v1, o2.v2]).to eq(-1)
+    expect([o1.v2, o1.v1] <=> [o3.v1, o3.v2]).to eq(-1)
+    expect([o1.v2, o1.v1] <=> [o3.v1, -o3.v2]).to eq(-1)
+    expect([o2.v2, o1.v1] <=> [-o2.v2, o2.v1]).to eq(1)
+    expect([o2.v2, o1.v1] <=> [-o2.v2, -o2.v1]).to eq(1)
+  end
+
+  # See https://ruby-doc.org/3.2.0/Enumerable.html#method-i-sort
   it "sorts by 2 keys, both ascending" do
-    result = (objs.sort { |a, b| [a.v1, a.v2] <=> [b.v1, b.v2] })
+    sort_lambda = ->(a, b) { [a.v1, a.v2] <=> [b.v1, b.v2] }
+    result = objs.sort(&sort_lambda)
     expect(result).to eq([o1, o2, o3, o4])
   end
 
+  it "sorts by 2 keys, both descending" do
+    sort_lambda = ->(a, b) { [-a.v1, -a.v2] <=> [-b.v1, -b.v2] }
+    result = objs.sort(&sort_lambda)
+    expect(result).to eq([o4, o3, o2, o1])
+  end
+
   it "sorts by 2 keys, first descending and second ascending" do
-    result = (objs.sort { |a, b| [b.v1, a.v2] <=> [a.v1, b.v2] })
+    sort_lambda = ->(a, b) { [-a.v1, a.v2] <=> [-b.v1, b.v2] }
+    result = objs.sort(&sort_lambda)
     expect(result).to eq([o4, o2, o3, o1])
   end
 
   it "sorts by 2 keys, first ascending and second descending" do
-    result = (objs.sort { |a, b| [a.v1, b.v2] <=> [b.v1, a.v2] })
+    sort_lambda = ->(a, b) { [a.v1, -a.v2] <=> [b.v1, -b.v2] }
+    result = objs.sort(&sort_lambda)
     expect(result).to eq([o1, o3, o2, o4])
   end
 end
