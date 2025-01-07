39b365855eb8e66569b14e20d7400ceb95fda8ce
Mike Slinn <mslinn@mslinn.com>
2025-01-07 10:00:06 -0500

    -

--- lib/util/mslinn_binary_search.rb
+++ lib/util/mslinn_binary_search.rb
@@ -1,6 +1,6 @@
 # Ruby's binary search is unsuitable because the value to be searched for changes the required ordering for String compares
 class MSlinnBinarySearch
-  attr_reader :array
+  attr_reader :array # For testing only
 
   def initialize(accessor_chain)
     @array = [] # Ordered highest to lowest
@@ -8,8 +8,8 @@ class MSlinnBinarySearch
   end
 
   # TODO: Cache this method
-  def suffix_binary_search(suffix)
-    low = @array.bsearch_index { |x| x.evaluate_with suffix }
+  def suffix_search(suffix)
+    low = search_index { |x| x.evaluate_with suffix }
     return [] if low.nil?
 
     high = low
@@ -18,20 +18,30 @@ class MSlinnBinarySearch
     @array[low..high]
   end
 
-  def binary_insert(url, new_item)
-    insert_at = @array.bsearch_index { |x| x.evaluate_with url }
+  # @return item from array that matches value
+  def find(value)
+    index = _find_index(value, 0, @array.length)
+    @array[index]
+  end
+
+  # @return index of matching value
+  def find_index(value)
+    _find_index(value, 0, @array.length)
+  end
+
+  def insert(lru_file)
+    insert_at = search_index(lru_file) { |x| x.evaluate_with lru_file.url }
     insert_at ||= 0
     @array.insert(insert_at, new_item)
   end
 
-  # @return index of matching val
-  def binary_search_index(target)
-    _binary_search(target, 0, @array.length)
+  def item_at(index)
+    @array[index]
   end
 
   private
 
-  def _binary_search(target, min_index, max_index)
+  def _find_index(target, min_index, max_index)
     mid_index = (min_index + max_index) / 2
     item = @array[mid_index]
     len = [item.length, target.length].min
@@ -40,10 +50,10 @@ class MSlinnBinarySearch
       mid_index
     when -1  # array[mid_index] < target
       min_index = mid_index + 1
-      _binary_search(target, min_index, max_index)
+      _find(target, min_index, max_index)
     when  1  # array[mid_index] > target
       max_index = mid_index - 1
-      _binary_search(target, min_index, max_index)
+      _find(target, min_index, max_index)
     end
   end
 end
--- spec/mslinn_binary_search_spec.rb
+++ spec/mslinn_binary_search_spec.rb
@@ -1,12 +1,65 @@
 require 'spec_helper'
 require_relative '../lib/util/mslinn_binary_search'
 
+sorted_strings = %w[aaa aab aac bbb bbc bbd ccc ccd cce]
+
+def insert_from_sorted_strings
+  string = sorted_strings.pop
+  msbs.insert LruFile.new(string, "Page #{string}") # { |x| x.url.start_with? string }
+end
+
 RSpec.describe(MSlinnBinarySearch) do
-  sorted_strings = %w[aaa aab aac bbb bbc bbd ccc ccd cce]
   msbs = described_class.new [:url, %i[start_with? placeholder]]
 
-  it 'returns index of first string match' do
-    index = msbs.binary_search_index 'a' # { |x| x.start_with? 'a' }
-    expect(sorted_strings[index]).to eq('aac')
+  it 'handles empty msbs.array' do
+    index = msbs.binary_search_index 'a' # { |x| x.url.start_with? 'a' }
+    expect(index).to be_nil
+  end
+
+  it 'inserts an item into msbs.array' do
+    insert_from_sorted_strings
+    expect(msbs.array.length).to be(1)
+  end
+
+  it 'inserts remaining items into msbs.array' do
+    insert_from_sorted_strings until sorted_strings.empty?
+    expect(msbs.array.length).to be(9)
+    expect(sorted_strings.length).to be_zero
+  end
+
+  it 'handles an empty search string by returning the index of the first item (0)' do
+    index = msbs.binary_search_index '' # { |x| x.url.start_with? '' }
+    expect(index).to be(0)
+
+    lru_file = msbs.item_at(index)
+    expect(lru_file.url).to be('aaa')
+  end
+
+  it 'returns the index of the first match' do
+    index = msbs.find_index 'a' # { |x| x.url.start_with? 'a' }
+    expect(index).to eq(0)
+
+    index = msbs.find_index 'aab' # { |x| x.url.start_with? 'aab' }
+    expect(index).to eq(1)
+
+    index = msbs.find_index 'c' # { |x| x.url.start_with? 'c' }
+    expect(index).to eq(6)
+
+    index = msbs.find_index 'cce' # { |x| x.url.start_with? 'cce' }
+    expect(index).to eq(8)
+  end
+
+  it 'returns the item of the first match' do
+    item = msbs.find 'a' # { |x| x.url.start_with? 'a' }
+    expect(item.url).to eq('aaa')
+
+    item = msbs.find 'aab' # { |x| x.url.start_with? 'aab' }
+    expect(item.url).to eq('aab')
+
+    item = msbs.find 'c' # { |x| x.url.start_with? 'c' }
+    expect(item.url).to eq('ccc')
+
+    item = msbs.find 'cce' # { |x| x.url.start_with? 'cce' }
+    expect(item.url).to eq('cce')
   end
 end
