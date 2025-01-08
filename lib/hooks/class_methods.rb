d2fef0223495e33bc10ffe7ec49ac7fa23d58335
Mike Slinn <mslinn@mslinn.com>
2025-01-07 19:40:47 -0500

    -

--- lib/hooks/class_methods.rb
+++ lib/hooks/class_methods.rb
@@ -27,7 +27,7 @@ module AllCollectionsHooks
                         AllCollectionsHooks.apages_from_objects(site.pages, 'individual_page')
     @everything       = @all_documents +
                         AllCollectionsHooks.apages_from_objects(site.static_files, 'static_file')
-    @sorted_lru_files = SortedLruFiles.new.add_pages @everything
+    # @sorted_lru_files = SortedLruFiles.new.add_pages @everything # Not working yet
 
     site.all_collections  = @all_collections
     site.all_documents    = @all_documents
