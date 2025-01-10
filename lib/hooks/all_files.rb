dc7a0c0c5cb00fe4f3832bb135b3f563be879ae3
Mike Slinn <mslinn@mslinn.com>
2025-01-10 10:52:27 -0500

    -

--- lib/hooks/all_files.rb
+++ lib/hooks/all_files.rb
@@ -2,16 +2,18 @@ require_relative '../util/send_chain'
 
 # Insert the url of a Jekyll::Page into each LruFile instance,
 # along with the Page reference
+# todo: replace references to url and :url with reverse_url and :reverse_url
 LruFile = Struct.new(:url, :page) do
   include SendChain
 end
 
 # Matches suffixes of an array of urls
+# Converts suffixes to prefixes
 class SortedLruFiles
   attr_reader :msbs
 
   def initialize
-    @msbs = MSlinnBinarySearch.new %i[url end_with?]
+    @msbs = MSlinnBinarySearch.new %i[url start_with?]
   end
 
   def add_pages(pages)
@@ -25,6 +27,6 @@ class SortedLruFiles
   end
 
   def select(suffix)
-    @msbs.suffix_binary_search suffix
+    @msbs.select_pages suffix
   end
 end
